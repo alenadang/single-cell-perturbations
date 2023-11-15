4acb82ba47ef501c2eb25d74a5d191bc36af88bb
codnoob11 <e0726406@u.nus.edu>
2023-11-15 18:25:04 +0800

    File_required

---
+++ Models/Files_Required/auxFunctions.py
@@ -0,0 +1,146 @@
+# functions from the kaggle notebook
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.metrics import mean_absolute_error
+
+def test(word):
+    print(word)
+
+def calculate_mae_and_mrrmse(model, data, y_true):
+    """
+    Calculate Mean Absolute Error (MAE) and Mean Rowwise Root Mean Squared Error (MRRMSE).
+
+    Parameters:
+    - model: The trained  model.
+    - data: The input data for prediction.
+    - y_true: The true target values.
+    - scaler: The scaler used for data normalization.
+
+    Returns:
+    - None
+    """
+    # Predict using the model
+    y_pred_original = model.predict(data, batch_size=1)
+    
+    # Calculate Mean Absolute Error (MAE)
+    mae = mean_absolute_error(y_true , y_pred_original)
+    
+    # Calculate Mean Rowwise Root Mean Squared Error (MRRMSE)
+    rowwise_rmse = np.sqrt(np.mean(np.square(y_true - y_pred_original), axis=1))
+    mrrmse_score = np.mean(rowwise_rmse)
+    
+    # Print the results
+    print(f"Mean Absolute Error (MAE): {mae}")
+    print(f"Mean Rowwise Root Mean Squared Error (MRRMSE): {mrrmse_score}")
+    
+def mean_rowwise_rmse_loss(y_true, y_pred):
+    """
+    Custom loss function to calculate the Mean Rowwise Root Mean Squared Error (RMSE) loss.
+
+    Parameters:
+    - y_true: The true target values.
+    - y_pred: The predicted values.
+
+    Returns:
+    - Mean Rowwise RMSE loss as a scalar tensor.
+    """
+    # Calculate RMSE for each row
+    rmse_per_row = tf.sqrt(tf.reduce_mean(tf.square(y_true - y_pred), axis=1))
+    # Calculate the mean of RMSE values across all rows
+    mean_rmse = tf.reduce_mean(rmse_per_row)
+    
+    return mean_rmse
+
+def custom_mean_rowwise_rmse(y_true, y_pred):
+    """
+    Custom metric to calculate the Mean Rowwise Root Mean Squared Error (RMSE).
+
+    Parameters:
+    - y_true: The true target values.
+    - y_pred: The predicted values.
+
+    Returns:
+    - Mean Rowwise RMSE as a scalar tensor.
+    """
+    # Calculate RMSE for each row
+    rmse_per_row = tf.sqrt(tf.reduce_mean(tf.square(y_true - y_pred), axis=1))
+    # Calculate the mean of RMSE values across all rows
+    mean_rmse = tf.reduce_mean(rmse_per_row)
+    
+    return mean_rmse
+
+def create_model_checkpoint(filepath, monitor='val_mae', save_best_only=True,
+                            save_weights_only=True, mode='auto', verbose=0):
+    """
+    Create a ModelCheckpoint callback for saving the best model weights during training.
+
+    Args:
+        filepath (str): Filepath to save the best weights.
+        monitor (str): Metric to monitor (e.g., 'val_loss' or 'val_mae').
+        save_best_only (bool): Save only the best weights.
+        save_weights_only (bool): Save only the model's weights, not the entire model.
+        mode (str): One of {'auto', 'min', 'max'}. In 'min' mode, it saves when the monitored metric decreases.
+        verbose (int): Verbosity mode. 0 = silent, 1 = progress bar, 2 = one line per epoch.
+
+    Returns:
+
+        keras.callbacks.ModelCheckpoint: ModelCheckpoint callback.
+    """
+    checkpoint = tf.keras.callbacks.ModelCheckpoint(
+        filepath=filepath,
+        monitor=monitor,
+        save_best_only=save_best_only,
+        save_weights_only=save_weights_only,
+        mode=mode,
+        verbose=verbose
+    )
+    return checkpoint
+
+def plot_training_history(history, metrics):
+    """
+    Plot training history curves for loss and evaluation metrics on the same line.
+
+    Args:
+        history (keras.callbacks.History): Training history object.
+        metrics (list): List of metric names to plot.
+
+    Returns:
+        None
+    """
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(len(loss))
+
+    plt.figure(figsize=(12, 6))
+
+    # Plot loss
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, loss, label='Training Loss', color="blue")
+    plt.plot(epochs, val_loss, label='Validation Loss', color="red")
+    plt.title('Loss')
+    plt.xlabel('Epochs')
+    plt.legend()
+
+    # Plot specified evaluation metrics on the same line
+    for metric in metrics:
+        train_metric_name = f'Training {metric.capitalize()}'
+        val_metric_name = f'Validation {metric.capitalize()}'
+        train_metric = history.history[metric]
+        val_metric = history.history['val_' + metric]
+
+        plt.subplot(1, 2, 2)
+        plt.plot(epochs, train_metric, label=train_metric_name, color="green")
+        plt.plot(epochs, val_metric, label=val_metric_name, color="orange")
+
+    plt.title('Metrics')
+    plt.xlabel('Epochs')
+    plt.legend(loc='upper right')
+
+    plt.tight_layout()
+    plt.show()
